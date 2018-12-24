bf64a7dc16ed438e887a7b02bc0ca4f4d5f284d7
xixihuang <xi.k.huang@gmail.com>
2018-12-24 21:46:07 +0800

    docs: add docs for cls.sh, hello_World.sh, task_Day.sh

---
+++ hello_World.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+a = "Hello World!"
+num = 2
+echo "a is : $a num is : ${num}nd"
---
+++ task_Day.sh
@@ -0,0 +1,8 @@
+#！/bin/sh
+
+#先设置语言为中文
+export LANG="zh_CN"
+#二
+data_today='date +%a'
+#2
+week_day='date +%W'
